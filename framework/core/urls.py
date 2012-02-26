19e87bad689ac6971749336f2bdbc0c32373af41
Isaac Bythewood <isaac@bythewood.me>
2012-02-26 17:10:33 -0500

    Cutting off my form submission view from my home view, no longer inlining everything to improve page load speed on form submission.

--- framework/core/urls.py
+++ framework/core/urls.py
@@ -3,4 +3,5 @@ from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns('core.views',
     url(r'^$', 'home', name='home'),
+    url(r'^contact/$', 'contact', name='contact')
 )
--- framework/core/views.py
+++ framework/core/views.py
@@ -1,9 +1,18 @@
 from django.template.response import TemplateResponse
 from django.core.mail import EmailMessage
 from django.contrib import messages
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
 
 
 def home(request):
+    template = 'core/home.html'
+    context = None
+
+    return TemplateResponse(request, template, context)
+
+
+def contact(request):
     if request.method == 'POST':
         subject = 'Pony From isaacbythewood.com'
         message = request.POST['message']
@@ -14,9 +23,7 @@ def home(request):
         email.send()
         
         messages.success(request, "<h1>The pony is on it's way!</h1> <p>No actual ponies were harmed in the sending of this message.</p>")
+    else:
+        messages.warning(request, "<h1>You're doing it wrong.</h1> <p>Please submit the form on the home page.</p>")
 
-    template = 'core/home.html'
-
-    context = None
-
-    return TemplateResponse(request, template, context)
+    return HttpResponseRedirect(reverse('core:home'))
