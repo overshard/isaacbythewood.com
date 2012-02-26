b2249463c31177e36c18f33ed92727c9becb9535
Isaac Bythewood <isaac@bythewood.me>
2012-02-26 16:59:02 -0500

    Now using the messages system for form submission response.

--- framework/core/views.py
+++ framework/core/views.py
@@ -1,12 +1,9 @@
 from django.template.response import TemplateResponse
 from django.core.mail import EmailMessage
+from django.contrib import messages
 
 
 def home(request):
-    context = {
-        'submitted': False,
-    }
-
     if request.method == 'POST':
         subject = 'Pony From isaacbythewood.com'
         message = request.POST['message']
@@ -16,8 +13,10 @@ def home(request):
         email = EmailMessage(subject, message, sender, recipient)
         email.send()
         
-        context['submitted'] = True
+        messages.success(request, "<h1>The pony is on it's way!</h1> <p>No actual ponies were harmed in the sending of this message.</p>")
 
     template = 'core/home.html'
+
+    context = None
 
     return TemplateResponse(request, template, context)
