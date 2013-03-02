2e490072c52a81f0cefb9174b8067b62245dcacf
Isaac Bythewood <isaac@bythewood.me>
2013-03-02 10:05:33 +0000

    Setup root static files and CNAME, add github importer to requirements, slight update to analytics include

--- Makefile
+++ Makefile
@@ -70,7 +70,7 @@ ftp_upload: publish
 	lftp ftp://$(FTP_USER)@$(FTP_HOST) -e "mirror -R $(OUTPUTDIR) $(FTP_TARGET_DIR) ; quit"
 
 github: publish
-	ghp-import $(OUTPUTDIR)
+	ghp-import -m "Update" $(OUTPUTDIR)
 	git push origin gh-pages
 
 .PHONY: html help clean regenerate serve devserver publish ssh_upload rsync_upload dropbox_upload ftp_upload github
--- pelicanconf.py
+++ pelicanconf.py
@@ -10,13 +10,24 @@ DEFAULT_LANG = u'en'
 
 DEFAULT_PAGINATION = 5
 DEFAULT_DATE_FORMAT = '%d %b %Y'
-GOOGLE_ANALYTICS_ID = 'UA-35235932-1'
 
 JINJA_EXTENSIONS = (
     'jinja2.ext.loopcontrols',
 )
 
 THEME = 'theme'
+
+FILES_TO_COPY = (
+    ('extra/CNAME', 'CNAME'),
+    ('extra/apple-touch-icon-114x114-precomposed.png', 'apple-touch-icon-114x114-precomposed.png'),
+    ('extra/apple-touch-icon-57x57-precomposed.png', 'apple-touch-icon-57x57-precomposed.png'),
+    ('extra/apple-touch-icon-72x72-precomposed.png', 'apple-touch-icon-72x72-precomposed.png'),
+    ('extra/apple-touch-icon.png', 'apple-touch-icon.png'),
+    ('extra/apple-touch-icon-precomposed.png', 'apple-touch-icon-precomposed.png'),
+    ('extra/favicon.ico', 'favicon.ico'),
+    ('extra/humans.txt', 'humans.txt'),
+    ('extra/robots.txt', 'robots.txt'),
+)
 
 LINKS =  (
     ('GitHub', 'github.com', 'https://github.com/overshard'),
