8d857b266312c9260e6efe11f453c477d3fba101
Isaac Bythewood <isaac@bythewood.me>
2018-02-18 03:47:49 -0500

    Remove buggy callback on ghpages deploy

--- gulpfile.js/tasks/deploy.js
+++ gulpfile.js/tasks/deploy.js
@@ -3,6 +3,6 @@ const gulp = require('gulp');
 const ghpages = require('gh-pages');
 
 
-gulp.task('deploy', ['build'], cb => {
-    return ghpages.publish('build', {}, cb);
+gulp.task('deploy', ['build'], () => {
+    return ghpages.publish('build');
 });
