3af7d2e005748a871efe4d2dd278d11dfc82950f
Isaac Bythewood <isaac@bythewood.me>
2021-04-06 00:21:43 +0000

    Swap back to webpack 4, adjust next-offline

--- next.config.js
+++ next.config.js
@@ -2,24 +2,15 @@ const withOffline = require("next-offline");
 
 const nextConfig = {
   target: "serverless",
-  future: {
-    webpack5: true,
-  },
   workboxOpts: {
-    swDest: "static/service-worker.js",
     runtimeCaching: [
       {
         urlPattern: /^https?.*/,
         handler: "NetworkFirst",
         options: {
-          cacheName: "https-calls",
-          networkTimeoutSeconds: 15,
+          cacheName: "offlineCache",
           expiration: {
-            maxEntries: 150,
-            maxAgeSeconds: 30 * 24 * 60 * 60
-          },
-          cacheableResponse: {
-            statuses: [0, 200]
+            maxEntries: 200
           }
         }
       }
