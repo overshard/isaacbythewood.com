ef277003b28d87bab1d2f99415fd6929c92ee485
Isaac Bythewood <isaac@bythewood.me>
2022-06-27 03:34:22 +0000

    Remove next-pwa

--- next.config.js
+++ next.config.js
@@ -1,21 +1,7 @@
 const withPlugins = require("next-compose-plugins");
-const withPWA = require("next-pwa");
-const runtimeCaching = require("next-pwa/cache");
 
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.NODE_ENV !== "development",
 });
 
-module.exports = withPlugins([
-  [
-    withPWA,
-    {
-      pwa: {
-        dest: "public",
-        disable: process.env.NODE_ENV === "development",
-        runtimeCaching,
-      },
-    },
-  ],
-  [withBundleAnalyzer],
-]);
+module.exports = withPlugins([[withBundleAnalyzer]]);
